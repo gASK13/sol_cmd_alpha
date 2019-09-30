849f41479a83cfb3228eabe61b46716779dd47b5
gask13 <gask13@gmail.com>
2019-09-30 16:51:03 +0200

    Health fix

--- src/mining.mjs
+++ src/mining.mjs
@@ -318,7 +318,9 @@ export class MiningStatus extends Phaser.Scene {
     }
     
     updateHealth() {
-        this.healthBar.setCrop(0, 0, 156*(this.pShip.health / this.pShip.maxHealth), 16);
-        this.shieldBar.setCrop(0, 0, 156*(this.pShip.shield / this.pShip.maxShield), 16);
+        if (this.pShip) { //don't update when ship is not there yet
+            this.healthBar.setCrop(0, 0, 156*(this.pShip.health / this.pShip.maxHealth), 16);
+            this.shieldBar.setCrop(0, 0, 156*(this.pShip.shield / this.pShip.maxShield), 16);
+        }
     }        
 }
