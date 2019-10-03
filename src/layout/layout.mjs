895b0bc27a8bf66c7d3b164948f4f2dda871c87b
gask13 <gask13@gmail.com>
2019-10-03 16:51:29 +0200

    Tooltip draft.

--- src/layout/layout.mjs
+++ src/layout/layout.mjs
@@ -28,56 +28,77 @@ export class ShipLayout extends Phaser.Scene {
     });
 
     // Draw inventory
-    this.add.text(900, 100, "INVENTORY").setOrigin(0.5, 0.5).setFontSize(20).setColor('white');
-    this.add.text(900, 120, "Drag & Drop").setOrigin(0.5, 0.5).setFontSize(10).setColor('white');
+    this.add.text(900, 50, "INVENTORY").setOrigin(0.5, 0.5).setFontSize(20).setColor('white');
+    this.add.text(900, 70, "Drag & Drop").setOrigin(0.5, 0.5).setFontSize(10).setColor('white');
 
-    var sy = 150;
+    var sy = 100;
+    var sx = 850;
     for (var item of this.player.inventory) {
       let self = this;
-      var spr = this.add.sprite(900, sy, item.key).setInteractive();
+      var spr = this.add.sprite(sx, sy, item.key).setInteractive();
       spr.item = item;
+      this.createTooltip(spr);
       this.input.setDraggable(spr);
-      this.input.on('dragstart' , function(pointer, object) {
-        object.originalX = object.x;
-        object.originalY = object.y;
-        object.originalA = object.rotation;
-        object.setRotation(0);
+      spr.on('pointerover',function(pointer){
+          this.tooltip.setVisible(true);
       });
-      this.input.on('drag' , function(pointer, object, x, y) {
-        object.x = x;
-        object.y = y;
-        object.setRotation(object.originalA);
-        object.setTint(0xffffff);
-        for (var hp of self.hardpoints) {
-          if(hp.getBounds().contains(x,y)) {
-            if (hp.hardpoint.accepts(object.item.itemClass)) {
-              object.x = hp.x;
-              object.y = hp.y;
-              object.setRotation(hp.hardpoint.angle);
-              object.setTint(0x00ff00);
-            } else {
-              object.setTint(0xff0000);
-            }
-          }
-        }
-      });
-      this.input.on('dragend' , function(pointer, object) {
-        object.setTint(0xffffff);
-        for (var hp of self.hardpoints) {
-          if(hp.getBounds().contains(object.x,object.y)) {
-            if (hp.hardpoint.accepts(object.item.itemClass)) {
-              object.x = hp.x;
-              object.y = hp.y;
-              object.setRotation(hp.hardpoint.angle);
-              self.player.shipInstance.loadout[hp.hardpoint.id] = object.item;
-              return;
-            }
-          }
-        }
-        object.x = object.originalX;
-        object.y = object.originalY;
+
+      spr.on('pointerout',function(pointer){
+        this.tooltip.setVisible(false);
       });
       sy += 50;
+      if (sy > 500) { sy = 100; sx += 50}
     }
+
+    this.input.on('dragstart' , function(pointer, object) {
+      object.originalX = object.x;
+      object.originalY = object.y;
+      object.originalA = object.rotation;
+      object.setRotation(0);
+      object.tooltip.setVisible(false);
+    });
+    this.input.on('drag' , function(pointer, object, x, y) {
+      object.x = x;
+      object.y = y;
+      object.setRotation(object.originalA);
+      object.setTint(0xffffff);
+      for (var hp of self.hardpoints) {
+        if(hp.getBounds().contains(x,y)) {
+          if (hp.hardpoint.accepts(object.item.itemClass)) {
+            object.x = hp.x;
+            object.y = hp.y;
+            object.setRotation(hp.hardpoint.angle);
+            object.setTint(0x00ff00);
+          } else {
+            object.setTint(0xff0000);
+          }
+        }
+      }
+    });
+    this.input.on('dragend' , function(pointer, object) {
+      object.setTint(0xffffff);
+      for (var hp of self.hardpoints) {
+        if(hp.getBounds().contains(object.x,object.y)) {
+          if (hp.hardpoint.accepts(object.item.itemClass)) {
+            object.x = hp.x;
+            object.y = hp.y;
+            object.setRotation(hp.hardpoint.angle);
+            self.player.shipInstance.loadout[hp.hardpoint.id] = object.item;
+            return;
+          }
+        }
+      }
+      object.x = object.originalX;
+      object.y = object.originalY;
+      if (object.getBounds().containst(pointer.x, pointer.y)) {
+        object.tooltip.setVisible(true);
+      }
+    });
+  }
+
+  createTooltip(sprite) {
+    var container = this.add.container(sprite.x - 20, sprite.y).setVisible(false);
+    container.add(this.add.text(0, 0, sprite.item.itemClass.name).setOrigin(1, 0.5));
+    sprite.tooltip = container;
   }
 }
